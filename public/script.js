436e08054ace85e8e4504c5b22dffdef71c34511
Wasif Zaman <teatocode@gmail.com>
2017-07-31 16:16:54 -0400

    Add some jquery sugar

--- public/script.js
+++ public/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $('.full').hide();
-  $('.details').on('click', function() {
-    $('.full').toggle();
+  $('.det-title').hide();
+  $('.details').click(function(e) {
+    e.preventDefault();
+    var details = $('.details');
+    details.text() == 'Details' ? details.text('X') : details.text('Details');
+    $('.det-title').slideToggle();
+    $('.full').slideToggle();
     $('html, body').animate({
-        scrollTop: $('.full').offset().top
+        scrollTop: $('.det-title').offset().top
     }, 500);
   })
 })
